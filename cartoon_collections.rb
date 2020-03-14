38cc36c53cf2ab9eb8292d458debfbde86f1f53d
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-14 02:57:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def roll_call_dwarves(array)  #code an argument here
+def roll_call_dwarves(array)  
   array.each.with_index(1) do |dwarf, i|
   puts "#{i} #{dwarf}"
 end
@@ -11,6 +11,10 @@ powers << power.capitalize+"!"
 end
 powers
 end
+
+def long_planeteer_calls(array)
+  
+
 
 #def find_the_cheese# code an argument here
   # the array below is here to help
