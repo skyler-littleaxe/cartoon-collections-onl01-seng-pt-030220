42b748d89dfff7c6f1f7599c220120305837bc64
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-14 00:37:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,15 +5,10 @@ end
 end
 
 def summon_captain_planet(array)
-
-  # Your code here
-#end
-
-#def long_planeteer_calls# code an argument here
-  # Your code here
-#end
+array.upcase
+end
 
 #def find_the_cheese# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese_types = ["cheddar", "gouda", "camembert"]
 #end
