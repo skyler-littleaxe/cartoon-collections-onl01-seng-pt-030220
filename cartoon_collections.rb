b0ffa66a476abbdc1b463f36fb1146a8a97fe33d
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-14 03:16:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(array)
  end
 
 def find_the_cheese(array)
-  array.find {|item| item.include?("cheddar")}
+  array.find("cheddar")
+  #{|item| item.include?("cheddar")}
   # the array below is here to help
   #cheese_types = ["cheddar", "gouda", "camembert"]
 end
